868dadae43b996d4a582c9e61a87bcb334efede7
DOMINICBAUTISTA <117502327+DOMINICBAUTISTA@users.noreply.github.com>
2023-02-08 23:46:27 +0800

    Add project files.

---
+++ LoginRegistration/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using LoginRegistration.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace LoginRegistration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+
+        public string Registration(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("ToysCon"));
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(UserName,Password,Email,IsActive) VALUES('" +registration.UserName+ "','" +registration.Password+ "','" +registration.Email+ "','" +registration.IsActive+ "')", con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return "Data Inserted";
+            }
+            else
+            {
+               return "Error!";
+            }
+        }
+    }
+}
---
+++ LoginRegistration/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginRegistration.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
